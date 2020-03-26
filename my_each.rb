4db21d9878d46d8d367d6c7d206109a317e16dbe
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 03:13:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,10 @@
-kitties = ["Max", "Peaches", "Gilly"]
 
-def my_each(kitties) do |i|
-  puts i
+
+def my_each
+kitties = ["Max", "Peaches", "Gilly"]
+  my_each(kitties) do |i|
+    puts i 
+    
+  end
   
 end
